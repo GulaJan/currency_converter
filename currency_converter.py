41838a7bae692aaa35c51d5a61629305bda665ac
jamal <gulajan95@gmail.com>
2018-02-20 18:04:15 +0100

    symbols and json added

--- currency_converter.py
+++ currency_converter.py
@@ -8,6 +8,7 @@ import sys
 import argparse
 import urllib.request
 import re
+import json
 
 class CLI:
 	def argParsing():	
@@ -17,10 +18,10 @@ class CLI:
 		parser.add_argument('--output_currency', action="store", dest="outCurr", help='Currency to convert to. A 3 letter name or the currency symbol.')
 		args = parser.parse_args()
 		if(args.inCurr == args.outCurr):
-			sys.stderr.write("Output currency has to differ from the input currency!")
+			sys.stderr.write("Output currency has to differ from the input currency!\n")
 			sys.exit(1)
 		if(args.amount < 0):
-			sys.stderr.write("Cannot convert a negative value!")
+			sys.stderr.write("Cannot convert a negative value!\n")
 			sys.exit(1)
 		return args
 
@@ -33,7 +34,7 @@ class CLI:
 
 	def filterData(inCurr, outCurr, data):
 		data = data.decode()
-		print(data)
+#		print(data)
 		pattern = "currency='([a-z,A-Z]{3})'\srate='([\d]+[.][\d]+)'"
 		result = dict()
 		matched = re.findall(pattern, data)
@@ -51,22 +52,90 @@ class CLI:
 				inRate = item[1]
 			if(item[0] == outCurr): #item[0] = currencies, item[1] = rates
 				outRate = item[1]
-		print(inRate)
-		print(outRate)
-		print(amount)
+
 		return amount / float(inRate) * float(outRate)
 
+	def decipherSymbol(symbol):
+		# Errors : Countries with same symbols but different rates ? [USD AUD CAD HKD MXN NZD SGD], [DKK, SEK, ISK, NOK], [JPY, CNY]
+		return{
+			chr(36): 'USD',
+#			chr(36): 'AUD',
+#			chr(36): 'CAD',
+#			chr(36): 'HKD',
+#			chr(36): 'MXN',
+#			chr(36): 'NZD',
+#			chr(36): 'SGD',
+			chr(165): 'JPY',
+#			chr(165): 'CNY',
+			chr(1083) + chr(1074): 'BGN',
+			chr(75) + chr(269): 'CZK',
+			chr(107) + chr(114): 'DKK',
+#			chr(107) + chr(114): 'SEK',
+#			chr(107) + chr(114): 'ISK',
+#			chr(107) + chr(114): 'NOK',
+			chr(163): 'GBP',
+			chr(70) + chr(116): 'HUF',
+			chr(122) + chr(322): 'PLN',
+			chr(108) + chr(101) + chr(105): 'RON',
+			chr(67) + chr(72) + chr(70): 'CHF',
+			chr(107) + chr(110): 'HRK',
+			chr(8381): 'RUB',
+			chr(8378): 'TRY',
+			chr(82) + chr(36): 'BRL',
+			chr(82) + chr(112): 'IDR',
+			chr(8362): 'ILS',
+			chr(8377): 'INR',
+			chr(8361): 'KRW',
+			chr(82) + chr(77): 'MYR',
+			chr(8369): 'PHP',
+			chr(3647): 'THB',
+			chr(82): 'ZAR',											
+		}[symbol]
 
 	arguments = argParsing()
 	data = fetchData()
 	filteredData = filterData(arguments.inCurr, arguments.outCurr, data)
-	res = convert(arguments.amount, arguments.inCurr, arguments.outCurr, filteredData)
-	print(res)
+
+	if((len(arguments.outCurr) != 3) or (not(arguments.outCurr.isupper()))):
+		try:
+			arguments.outCurr = decipherSymbol(arguments.outCurr)
+		except:
+			sys.stderr.write("Output symbol was not recognized!\n")
+			sys.exit(3)
+
+	if((len(arguments.inCurr) != 3) or (not(arguments.inCurr.isupper()))):
+		try:
+			arguments.inCurr = decipherSymbol(arguments.inCurr)
+		except(KeyError):
+			sys.stderr.write("Input symbol was not recognized!\n")
+			sys.exit(3)
+
+	if(arguments.outCurr == None):
+		res = convert(arguments.amount, arguments.inCurr, 'EUR', filteredData)
+#		print(res)
+		for item in filteredData:
+			try:
+				res = convert(arguments.amount, arguments.inCurr, item[0], filteredData)
+			except(UnboundLocalError):
+				sys.stderr.write("Input or output currency was not recognized!\n")
+				sys.exit(3)
+
+#			print(res)
+	else:
+		try:
+			res = convert(arguments.amount, arguments.inCurr, arguments.outCurr, filteredData)
+		except(UnboundLocalError):
+			sys.stderr.write("Input or output currency was not recognized!\n")
+			sys.exit(3)
+
+#	print("%.2f" % round(res,2))
+#	res = str(round(res,2))
+	res = "%.2f" % res 
+#	print(arguments.outCurr)
+	print(json.dumps({'input': {'amount': arguments.amount, 'currency': arguments.inCurr}, 'output': {arguments.outCurr : res}}, indent = 4, sort_keys=True))
 	sys.exit(0)
 
 
 	# TO DO:
-	# 1. CURRENCY SYMBOLS
-	# 2. NO OUTPUT
-	# 3. CONVERTING EUROS
+	# 3. JSON
 	# 4. FULL API
